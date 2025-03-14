ce2b620b8bc85f598a0e1695f4b0816aff4ea6a1
athom031 <alexjosthomas@gmail.com>
2025-03-14 16:51:01 -0700

    added move logic

--- src/directory.js
+++ src/directory.js
@@ -9,12 +9,21 @@ class Directory {
     }
 
     /**
+     * Helper function to scalably get parts of path 
+     * @param {string} path
+     * @return {string[]} - The parts of path
+     */
+    _splitPath(path) {
+        return path.split('/');
+    }
+
+    /**
      * Create path in direcotry tree 
      * If nodes along the way dont exist in tree to make valid path, make them 
      * @param {string} path - The path to create (ie: 'fruits/apples/fuji)
      */
     create(path) {
-        const parts = path.split('/');
+        const parts = this._splitPath(path);
 
         let current = this.root;
 
@@ -47,10 +56,50 @@ class Directory {
             this.list(child, depth + 1);
         }
     }
+
+    /**
+     * Move a node to a new parent node 
+     * @param {string} fromPath - The path of node to move (ie: 'fruits/apples/fuji')
+     * @param {string} toPath - The path of new parent node (ie: 'fruits')
+     */
+    move(fromPath, toPath) {
+        // Go through given from path
+        const fromParts = this._splitPath(fromPath);
+        let currFromNode = this.root;
+        for (const part of fromParts) {
+            if(currFromNode.hasChild(part)) {
+                currFromNode = currFromNode.children.get(part);
+            } else {
+                console.error(`ERROR: Cannot move ${fromPath} to ${toPath}.\n${fromPath} is not valid path in current directory.`);
+                return;
+            }
+        } 
+
+        // Go through given to path 
+        const toParts = this._splitPath(toPath);
+        let currToNode = this.root;
+        for (const part of toParts) {
+            if(currToNode.hasChild(part)) {
+                currToNode = currToNode.children.get(part);
+            } else {
+                console.error(`ERROR: Cannot move ${fromPath} to ${toPath}.\n${toPath} is not valid path in current directory.`);
+                return;
+            }
+        }
+
+        // So we traversed the from node path now lets delete this child from the parent so we can properly move it 
+        currFromNode.parent.removeChild(currFromNode.name);
+
+        // Now we can properly move the node to its new parent 
+        currToNode.addChild(currFromNode);
+    }
 }
 
 const tree = new Directory();
 tree.create('fruits');
 tree.create('fruits/apples');
 tree.create('fruits/apples/fuji');
+tree.create('vegetables');
+tree.list();
+tree.move('fruits/apples/fuji', 'vegetables');
 tree.list();
